cf27f2b7064fd4cdd8dd2719a9beda364c7e3a3a
shivam_since2002 <sg2109964@gmail.com>
2024-04-21 20:20:01 +0530

    21 April

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:fluttre_one/Widget%20Practice/SnackBar/SnackBarr.dart';
+import 'package:fluttre_one/Widget%20Practice/Alert/alert.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SnackBarr(),
+      home: ALert(),
     );
   }
 }
