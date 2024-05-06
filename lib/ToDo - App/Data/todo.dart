7781c46adf3de666ff133cf39a37cdc6b9ae5210
shivam_since2002 <sg2109964@gmail.com>
2024-05-06 09:56:03 +0530

    6 May

---
+++ lib/ToDo - App/Data/todo.dart
@@ -0,0 +1,10 @@
+class Todo {
+  final String title;
+  final String subtitle;
+  bool isDone;
+
+  //Creating Constructor
+  Todo({this.title = '', this.subtitle = '', this.isDone = false});
+
+  
+}
