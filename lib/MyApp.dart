3fa3ee13ee581baffa0307d82c623e293dd3e8bf
shivam_since2002 <sg2109964@gmail.com>
2024-05-06 17:56:41 +0530

    6 May

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:fluttre_one/Widget%20Practice/Alert/alert1.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttre_one/ToDo%20-%20App/bloc/todo_bloc.dart';
+import 'package:fluttre_one/ToDo%20-%20App/home.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Alertt1(),
+    return BlocProvider(
+      create: (context) => TodoBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Todo App",
+        theme: ThemeData(
+          colorScheme: const ColorScheme.light(
+            brightness: Brightness.light,
+            primary: Colors.yellowAccent,
+            onPrimary: Colors.black,
+            secondary: Colors.lightGreen,
+            onSecondary: Colors.lightGreen,
+            error: Colors.red,
+            onError: Colors.redAccent,
+            background: Colors.white,
+            onBackground: Colors.black,
+            surface: Colors.white54,
+            onSurface: Colors.white30,
+          ),
+        ),
+        home: BlocProvider<TodoBloc>(
+          create: (context) => TodoBloc()
+            ..add(
+              TodoStarted(),
+            ),
+          child: const TodoHome(),
+        ),
+      ),
     );
   }
 }
