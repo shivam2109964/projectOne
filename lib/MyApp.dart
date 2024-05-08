461e252122adcfcdb4a3c70d256797e05153a376
shivam_since2002 <sg2109964@gmail.com>
2024-05-08 18:06:18 +0530

    8 May

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttre_one/Projects/StatefulBuilder/StatefulBuild.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -8,7 +7,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Todo App",
-      home: StatefulBuildHome(),
+      home: null,
     );
   }
 }
