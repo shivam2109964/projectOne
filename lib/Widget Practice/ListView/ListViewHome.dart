c24a49f2a9c03fc957f1be4ce64dd27338506376
shivam_since2002 <sg2109964@gmail.com>
2024-05-09 12:36:12 +0530

    ListViewBuilder part 2

--- lib/Widget Practice/ListView/ListViewHome.dart
+++ lib/Widget Practice/ListView/ListViewHome.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ListViewHome extends StatelessWidget {
-  final List<int> colorCodes = <int>[
+  ListViewHome({super.key});
+
+  final List<int> colorCode = <int>[
     50,
     100,
     200,
@@ -11,19 +13,54 @@ class ListViewHome extends StatelessWidget {
     600,
     700,
     800,
-    900
+    900,
   ];
-  ListViewHome({super.key});
 
-  Widget _listBuilder() {
+  final List<List<int>> rgbColorCode = <List<int>>[
+    [255, 0, 0, 1],
+    [255, 165, 0, 1],
+    [255, 255, 0, 1],
+    [0, 128, 0, 1],
+    [0, 0, 255, 1],
+    [75, 0, 130, 1],
+    [128, 0, 128, 1],
+    [255, 20, 147, 1],
+    [0, 255, 255, 1],
+    [255, 0, 255, 1],
+  ];
+
+  Widget listBuilderLeft() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8.0),
+      itemCount: 500,
+      itemBuilder: (BuildContext context, int index) {
+        final List<int> colorcodeRGB =
+            rgbColorCode[index % rgbColorCode.length];
+        return Container(
+          color: Color.fromRGBO(
+            colorcodeRGB[0],
+            colorcodeRGB[1],
+            colorcodeRGB[2],
+            colorcodeRGB[3].toDouble(),
+          ),
+          child: Center(
+            child: Text("Item $index"),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget listBuilderRight() {
     return ListView.builder(
       padding: const EdgeInsets.all(8.0),
       itemCount: 500,
       itemBuilder: (BuildContext context, int index) {
         return Container(
-          height: 50,
-          color: Colors.purple[colorCodes[index % 10]],
-          child: Center(child: Text("Item $index")),
+          color: Colors.purple[colorCode[index % 10]],
+          child: Center(
+            child: Text("Item $index"),
+          ),
         );
       },
     );
@@ -32,21 +69,19 @@ class ListViewHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Scaffold(
-        appBar: AppBar(
-          title: const Text("ListView Builder"),
+      appBar: AppBar(
+        title: const Text("ListView Builder"),
+      ),
+      body: ConstrainedBox(
+        constraints: const BoxConstraints(
+          minHeight: 50,
+          maxHeight: 800,
         ),
-        body: Container(
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(
-              minHeight: 35.0,
-              maxHeight: 800.0,
-            ),
-            child: Row(
-              children: <Widget>[Expanded(child: _listBuilder())],
-            ),
-          ),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: listBuilderLeft()),
+            Expanded(child: listBuilderRight())
+          ],
         ),
       ),
     );
